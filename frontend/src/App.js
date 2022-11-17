b30baf1fb2234562f910648527c2b07e68ab0ab4
Ahmed <>
2022-11-17 22:02:19 +0300

    create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'>Marka Cadey Store</a>
+      </header>
+      <main>
+       Product 
+      </main>
+    </div>
+  );
+}
+
+export default App;
