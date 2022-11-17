b2abc3357206671cffc9916472da5e0d537c7256
Ahmed <>
2022-11-18 02:59:36 +0300

    add navbar bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,39 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import { LinkContainer } from 'react-router-bootstrap';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import StorefrontIcon from '@mui/icons-material/Storefront';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className='d-flex flex-column site-container'>
-
-
+      <div className="d-flex flex-column site-container">
         <header>
           <Navbar bg="dark" variant="dark">
             <Container>
               <LinkContainer to="/">
                 <Navbar.Brand>Marka Cadey Store</Navbar.Brand>
               </LinkContainer>
+              <Nav className=" text-center">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#Spicy">Spicy Sauce</Nav.Link>
+                <Nav.Link href="#sambusa">Sambusa</Nav.Link>
+                <Nav.Link href="#aboutUs">About Us</Nav.Link>
+                <Nav.Link href="#contactUs">Contact Us</Nav.Link>
+              </Nav>
+
+              <Nav>
+                <Nav.Link href="#deets"><ShoppingCartIcon/></Nav.Link>
+                <Nav.Link eventKey={2} href="#memes">
+                  <StorefrontIcon/>
+                </Nav.Link>
+              </Nav>
             </Container>
-          </Navbar> 
+          </Navbar>
         </header>
 
         <main>
@@ -30,14 +45,9 @@ function App() {
           </Container>
         </main>
 
-
         <footer>
-          <div className="text-center">
-            All rights reserved
-          </div>
+          <div className="text-center">All rights reserved</div>
         </footer>
-
-
       </div>
     </BrowserRouter>
   );
