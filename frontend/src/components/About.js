612f035b20cb59f20221d852f7ed5fd83db0285f
Ahmed <>
2022-11-26 01:08:25 +0300

    about page created

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,7 +1,22 @@
+import styles from '../styles/About.css'
 function About() {
   return (
-    <div>
-      <h1>About Us</h1>
+    <div class="about-container">
+      <div class="about-child">
+        <h2 class="green">About Us</h2>
+      </div>
+      <ul>
+          <li>1</li>
+          <p>hello</p>
+          <li>1</li>
+          <p>hello</p>
+          <li>1</li>
+          <p>hello</p>
+          <li>1</li>
+          <p>hello</p>
+          <li>1</li>
+          <p>hello</p>
+        </ul>
     </div>
   );
 }
